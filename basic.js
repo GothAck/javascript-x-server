6f721cf9e462468f07ac2e949174804031112e71
Greg Miell <greg@greg-net.co.uk>
2012-12-30 14:26:55 +0000

    Beginnings of JavaScript X Server prototype

---
+++ basic.js
@@ -0,0 +1,178 @@
+var net = require('net')
+  , x_types = require('./x_types');
+
+net.createServer(function (socket) {
+  new XServerClient(socket);
+}).listen(6041);
+
+
+function XServerClient (socket) {
+  this.socket = socket;
+  this.state = 0;
+  this.endian = null;
+  this.release = 11300000;
+  this.resource_id_mask = 0x001fffff;
+  this.resource_id_base = 0x00200000;
+  this.vendor = 'JavaScript X';
+  this.motion_buffer_size = 0xff;
+  this.maximum_request_length = 0xffff;
+  this.formats = [
+      new x_types.Format(0x01, 0x01, 0x20)
+    , new x_types.Format(0x04, 0x08, 0x20)
+    , new x_types.Format(0x08, 0x08, 0x20)
+    , new x_types.Format(0x0f, 0x10, 0x20)
+    , new x_types.Format(0x10, 0x10, 0x20)
+    , new x_types.Format(0x18, 0x20, 0x20)
+    , new x_types.Format(0x20, 0x20, 0x20)
+  ];
+  this.screens = [
+      new x_types.Screen(
+          0x00000026 // root
+        , 0x00000022 // def colormap
+        , 0x00ffffff // white
+        , 0x00000000 // black
+        , 0x00000000 // current input masks
+        , 400 // width px
+        , 400 // height px
+        , 400 // width mm
+        , 400 // height mm
+        , 0x0001 // min maps
+        , 0x0001 // max maps
+        , 0x20 // root visual
+        , 0 // backing stores
+        , 0 // save unders
+        , 0x18 //24 default depth
+        , [ // depths
+              new x_types.Depth(
+                  0x01 // depth 1
+              )
+            , new x_types.Depth(
+                  0x18 // depth 24
+                , [ // visualtypes
+                      new x_types.VisualType(
+                          0x00000020 // id
+                        , 0x04 // class
+                        , 0x08 // bits per rgb
+                        , 0x0100 // colormap entries
+                        , 0x00ff0000 // red mask
+                        , 0x0000ff00 // green mask
+                        , 0x000000ff // blue mask
+                      )
+                    , new x_types.VisualType(
+                          0x00000021 // id
+                        , 0x05 // class
+                        , 0x08 // bits per rgb
+                        , 0x0100 // colormap entries
+                        , 0x00ff0000 // red mask
+                        , 0x0000ff00 // green mask
+                        , 0x000000ff // blue mask
+                      )
+                  ]
+              )
+          ]
+      )
+  ];
+  socket.on('data', this.processData.bind(this));
+}
+
+XServerClient.prototype.processData = function (data) {
+  console.log('DATA');
+  data.endian = this.endian;
+  switch (this.state) {
+    case 0:
+      return this.setup(data);
+    case 1:
+      var req = new x_types.Request(data);
+      var func = this[XServerClient.opcodes[req.opcode]];
+      func && func.call(this, req);
+  }
+}
+
+XServerClient.prototype.setup = function (data) {
+  data.endian = this.endian = data[0] !== 'B' ? 'LE' : 'BE';
+  this.protocol_major = data.readUInt16(2);
+  this.protocol_minor = data.readUInt16(4);
+
+  this.auth_name = data.toString(
+      'ascii'
+    , 12
+    , 12 + data.readUInt16(6)
+  );
+
+  this.auth_data = data.toString(
+      'ascii'
+    , 12 + data.readUInt16(6) + 2
+    , 12 + data.readUInt16(6) + 2 + data.readUInt16(8)
+  );
+  console.log(this.auth_name, this.auth_data);
+
+  /* Deny connection
+  var reason = 'No Way'
+    , pad = 4 - ((reason.length % 4) || 4);
+
+  var res = new Buffer(8 + reason.length + pad);
+  res.fill(0);
+  res.writeUInt8(reason.length, 1); // 2 (0: 0, 1: reason.length)
+  this.writeUInt16(res, this.protocol_major, 2); // 2
+  this.writeUInt16(res, this.protocol_minor, 4); // 2
+  this.writeUInt16(res, (reason.length + pad) / 4, 6);
+  res.write(reason, 8, reason.length, 'ascii');
+  this.socket.write(res);
+  */
+  // Allow connection
+
+  var res = new Buffer(500);
+  res.endian = this.endian;
+  res.fill(0);
+  res.writeUInt8(1, 0);
+  res.writeUInt16(this.protocol_major, 2);
+  res.writeUInt16(this.protocol_minor, 4);
+  res.writeUInt16((32 + (this.formats.byteLength()) + stringPad(this.vendor) + this.screens.byteLength()) / 4, 6);
+  res.writeUInt32(this.release, 8);
+  res.writeUInt32(this.resource_id_base, 12);
+  res.writeUInt32(this.resource_id_mask, 16);
+  res.writeUInt32(this.motion_buffer_size, 20);
+  res.writeUInt16(this.vendor.length, 24);
+  res.writeUInt16(this.maximum_request_length, 26);
+  res.writeUInt8(this.screens.length, 28); // Number screens
+  res.writeUInt8(this.formats.length, 29); // Number formats
+  res.writeUInt8(0, 30); // image lsb first
+  res.writeUInt8(0, 31); // pixmap lsb first
+  res.writeUInt8(32, 32); // pixmap scanline unit
+  res.writeUInt8(32, 33); // pixmap scanline pad
+  res.writeUInt8(8, 34); // min keycode
+  res.writeUInt8(255, 35); // max keycode
+  // 4 unused
+  res.write(this.vendor, 40, this.vendor.length, 'ascii');
+  var base = 40 + stringPad(this.vendor);
+  base = this.formats.writeBuffer(res, base);
+  base = this.screens.writeBuffer(res, base);
+  var _res = new Buffer(base);
+  _res.fill(0);
+  res.copy(_res, 0, 0, base);
+  this.socket.write(_res);
+  console.log('Finished setup');
+  this.state = 1;
+}
+
+XServerClient.opcodes = {
+    98: 'QueryExtension'
+}
+
+XServerClient.prototype.QueryExtension = function (req) {
+  console.log('QE');
+  var res = new x_types.Reply(req);
+  res.data.writeUInt8(0, 0);
+  res.data.writeUInt8(req.opcode, 1);
+  res.data.writeUInt8(0, 2);
+  res.data.writeUInt8(0, 3);
+  var _res = new Buffer(res.length);
+  _res.endian = this.endian;
+  console.log(this.endian);
+  res.writeBuffer(_res, 0);
+  this.socket.write(_res);
+}
+
+function stringPad (string) {
+  return string.length + (4 - ((string.length % 4) || 4));
+}
---
+++ x_types.js
@@ -0,0 +1,166 @@
+Buffer.prototype.endian = null
+
+Object.keys(Buffer.prototype).filter(function (name) { return /(BE|LE)$/.test(name) }).map(function (name) { return name.slice(0, -2) })
+  .forEach(function (name) {
+    Buffer.prototype[name] = function () {
+      if (! this.endian) throw new Error('No endian');
+      return this[name + this.endian].apply(this, arguments);
+    }
+  });
+
+var awb_open = 0;
+Array.prototype.writeBuffer = function (buffer, offset) {
+  this.forEach(function (item) {
+    offset = item.writeBuffer(buffer, offset);
+  });
+  return offset;
+}
+
+Array.prototype.byteLength = function () {
+  return this.reduce(function (p, c) { return p + c.length }, 0);
+}
+
+function Format (depth, bpp, scanline_pad) {
+  this.depth = depth || 0;
+  this.bpp = bpp || 0;
+  this.scanline_pad = scanline_pad || 0;
+  this.length = 8;
+}
+
+module.exports.Format = Format;
+
+Format.prototype.writeBuffer = function (buffer, offset) {
+  buffer.writeUInt8(this.depth        , offset);
+  buffer.writeUInt8(this.bpp          , offset + 1);
+  buffer.writeUInt8(this.scanline_pad , offset + 2);
+  // 5 unused
+  return offset + this.length;
+}
+
+function VisualType (visualid, _class, bits_per_rgb, colormap_entries, red_mask, green_mask, blue_mask) {
+  this.visualid = visualid || 0;
+  this.class = _class || 0;
+  this.bits_per_rgb = bits_per_rgb || 0;
+  this.colormap_entries = colormap_entries || 0;
+  this.red_mask = red_mask || 0;
+  this.green_mask = green_mask || 0;
+  this.blue_mask = blue_mask || 0;
+  this.length = 24;
+}
+
+module.exports.VisualType = VisualType;
+
+VisualType.prototype.writeBuffer = function (buffer, offset) {
+  buffer.writeUInt32(this.visualid, offset); // 4
+  buffer.writeUInt8(this.class, offset + 4); // 1
+  buffer.writeUInt8(this.bits_per_rgb, offset + 5); // 1
+  buffer.writeUInt16(this.colormap_entries, offset + 6); // 2
+  buffer.writeUInt32(this.red_mask, offset + 8); // 4
+  buffer.writeUInt32(this.green_mask, offset + 12); // 4
+  buffer.writeUInt32(this.blue_mask, offset + 16); // 4
+  // 4 unused
+  return offset + this.length;
+}
+
+function Depth (depth, visual_types) {
+  this.depth = depth || 0;
+  this.visual_types = visual_types || [];
+}
+
+module.exports.Depth = Depth;
+
+Depth.prototype.__defineGetter__('length', function () {
+  return 8 + this.visual_types.reduce(function (p, c) { return p + c.length }, 0);
+});
+
+Depth.prototype.writeBuffer = function (buffer, offset) {
+  buffer.writeUInt8(this.depth, offset);
+  // 1 unused
+  buffer.writeUInt16(this.visual_types.length, offset + 2);
+  // 4 unused
+  offset += 8;
+  offset = this.visual_types.writeBuffer(buffer, offset);
+  return offset;
+}
+
+function Screen (window, colormap, white, black, current_input_masks, width_px, height_px, width_mm, height_mm, maps_min, maps_max, root_visual, backing_stores, save_unders, root_depth, depths) {
+  this.window = window || 0;
+  this.colormap = colormap || 0;
+  this.white = white || 0;
+  this.black = black || 0;
+  this.current_input_masks = current_input_masks || 0;
+  this.height_px = height_px || 0;
+  this.width_px = width_px || 0;
+  this.height_mm = height_mm || 0;
+  this.width_mm = width_mm || 0;
+  this.maps_min = maps_min || 0;
+  this.maps_max = maps_max || 0;
+  this.root_visual = root_visual || 0;
+  this.backing_stores = backing_stores || 0;
+  this.save_unders = save_unders || 0;
+  this.root_depth = root_depth || 0;
+  this.depths = depths || [];
+}
+
+module.exports.Screen = Screen;
+
+Screen.prototype.__defineGetter__('length', function () {
+    return 40 + this.depths.reduce(function (p, c) { return p + c.length }, 0);
+});
+
+Screen.prototype.writeBuffer = function (buffer, offset) {
+  buffer.writeUInt32(this.window, offset);
+  buffer.writeUInt32(this.colormap, offset + 4);
+  buffer.writeUInt32(this.white, offset + 8);
+  buffer.writeUInt32(this.black, offset + 12);
+  buffer.writeUInt32(this.current_input_masks, offset + 16);
+  buffer.writeUInt16(this.height_px, offset + 20);
+  buffer.writeUInt16(this.width_px, offset + 22);
+  buffer.writeUInt16(this.height_mm, offset + 24);
+  buffer.writeUInt16(this.width_mm, offset + 26);
+  buffer.writeUInt16(this.maps_min, offset + 28);
+  buffer.writeUInt16(this.maps_max, offset + 30);
+  buffer.writeUInt32(this.root_visual, offset + 32);
+  buffer.writeUInt8(this.backing_stores, offset + 36);
+  buffer.writeUInt8(this.save_unders, offset + 37);
+  buffer.writeUInt8(this.root_depth, offset + 38);
+  buffer.writeUInt8(this.depths.length, offset + 39);
+  return this.depths.writeBuffer(buffer, offset + 40);
+}
+
+function Request (data) {
+  this.opcode = data.readUInt8(0);
+  this.data_byte = data.readUInt8(1);
+  this.length_quad = data.readUInt16(2);
+  this.length = this.length_quad * 4;
+  this.data = data.slice(4);
+  this.sequence = Request.sequence ++;
+}
+
+Request.sequence = 1;
+
+module.exports.Request = Request;
+
+function Reply (request) {
+  this.opcode = request.opcode;
+  this.sequence = request.sequence;
+  this.data_byte = 0;
+  this.data = new Buffer(24);
+  this.data.fill(0);
+  this.data_extra = [];
+}
+
+module.exports.Reply = Reply;
+
+Reply.prototype.__defineGetter__('length', function () {
+  return 32 + this.data_extra.byteLength();
+});
+
+Reply.prototype.writeBuffer = function (buffer, offset) {
+  buffer.writeUInt8(1, offset);
+  buffer.writeUInt8(this.data_byte, offset + 1);
+  buffer.writeUInt16(this.sequence, offset + 2);
+  buffer.writeUInt32(this.data_extra.byteLength() / 4, offset + 4);
+  this.data.copy(buffer, offset + 8);
+  return this.data_extra.writeBuffer(buffer, offset + 8 + 24);
+}
